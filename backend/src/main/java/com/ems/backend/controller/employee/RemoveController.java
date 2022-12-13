cff71653b90567263ca0328c6e75fb5505468456
bz2021 <bz2021@mail.sdu.edu.cn>
2022-12-13 20:50:00 +0800

    update jar

--- backend/src/main/java/com/ems/backend/controller/employee/RemoveController.java
+++ backend/src/main/java/com/ems/backend/controller/employee/RemoveController.java
@@ -3,6 +3,7 @@ package com.ems.backend.controller.employee;
 import com.ems.backend.service.employee.RemoveEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -13,8 +14,8 @@ public class RemoveController {
     @Autowired
     private RemoveEmployeeService removeEmployeeService;
 
-    @PostMapping("/employee/delete")
-    public Map<String, String> removeEmployee(Map<String, String> data){
+    @PostMapping("/employee/delete/")
+    public Map<String, String> removeEmployee(@RequestParam Map<String, String> data){
         return removeEmployeeService.RemoveEmployee(data);
     }
 }
